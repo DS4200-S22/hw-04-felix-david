040dcd881a0dfc77ec74247d73a1f2cbcd2280b7
d-wei-leigh <2davidleigh@gmail.com>
2022-02-14 14:45:09 -0500

    clickable circle

---
+++ main.js
@@ -0,0 +1,10 @@
+
+// Counter function for circle 1
+
+var counter = 0;
+function circle1Clicked() {
+	document.getElementById("counter").innerHTML = counter+1;
+	counter += 1;
+	console.log(counter);
+}
+
