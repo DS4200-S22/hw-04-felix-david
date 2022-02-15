ed7970d92afcb5f99e5c0e35cc1054c3cc9516cf
d-wei-leigh <2davidleigh@gmail.com>
2022-02-15 10:34:48 -0500

    border appears w/ mouseover

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 
-// Counter function for circle 1
+// Counter function for circle1
 
 var counter = 0;
 function circle1Clicked() {
 	document.getElementById("counter").innerHTML = counter+1;
 	counter += 1;
 	console.log(counter);
+}
+
+// Mouseover and Mouseout events for circle2
+document.getElementById("circle2").onmouseover = function() {addBorder()};
+document.getElementById("circle2").onmouseout = function() {removeBorder()};
+
+function addBorder() {
+  document.getElementById("circle2").style.stroke = "black";
+  document.getElementById("circle2").style.strokeWidth = "5px";
+    console.log("moused over");
+}
+
+function removeBorder() {
+  document.getElementById("circle2").style.strokeWidth = "0px";
 }
 
